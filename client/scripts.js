4f6f42417e0472d2877264411f91be6e6bf7e585
cerise-at <cerise.athompson@gmail.com>
2021-08-04 11:01:58 +0100

    added new event handlers to create a page reload, added tests to make sure everything is there in teh html form, added functions to reload page to scripts file

--- client/scripts.js
+++ client/scripts.js
@@ -1,3 +1,8 @@
+//function to handle submit and page reload
+function handleSubmitReload(event){
+    searchHandler(event)
+    loadResults(event)
+}
 
 function searchHandler(event){
     event.preventDefault();
@@ -16,27 +21,25 @@ function searchHandler(event){
 
 function sortResults(data) {
     //loop over first 10 results
-    for(let i=0; i< 10; i++) 
-    {
+    for(let i=0; i< 10; i++) {
         //see if it has a cahce id:
-        if (data.items[i].cacheId = true)
-        {
+        if (data.items[i].cacheId = true){
         //give result a name from cacheId
             let a = data.items[i].cacheId
-            
             //make it a new class and add the data
             a = new Result(`${data.items[i].title}`, `${data.items[i].snippet}`, `${data.items[i].formattedUrl}`);
             finalResults[i] = a
-            console.log(finalResults);
-        // give Result a name from metadata
+            
         } else {
+            // give Result a name from metadata
             let b = data.items[i].pagemap.thing.name
+
             //make new class and add data
             b = new Result(`${data.items[i].title}`, `${data.items[i].snippet}`, `${data.items[i].formattedUrl}`);
+            
             //add results object to final result object
             finalResults[i] = b
         }
-      
     } 
 }
 
@@ -49,9 +52,22 @@ class Result {
     }
 }
 
-//at some point put the objects all into this object so that we can export it to the html file all in one go 
-//at the moment they are in ten seperate objects!
+//object for all the results to be wrapped in
 let finalResults = {}
+
+
+//RELOAD PAGE FUNC
+function luckyWebsiteReroute(event) {
+    event.preventDefault()
+    searchHandler(event)
+    let newPage = finalResults[0].formattedUrl
+    window.location.replace(`${newPage}`);
+
+}
+
+//function to chose top one from the object
+
+
 
 //keys for search
 let cx = '6bec81dadc88ed1d3'
@@ -59,5 +75,9 @@ let API_KEY = 'AIzaSyBLQ6G4cY74__pZ0pfUiBV7qlBgxbssheo'
 
 //watch for submission of form
 const form = document.getElementById('form');
-form.addEventListener('submit', searchHandler);
+form.addEventListener('submit', handleSubmitReload);
+form.addEventListener('click', luckyWebsiteReroute )
 
+module.exports = {
+    searchHandler
+}
