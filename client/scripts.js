8ade347c9b680aa744de01816f2a66b05407a207
cerise-at <cerise.athompson@gmail.com>
2021-08-04 11:27:23 +0100

    function to sort search results into an object completed

--- client/scripts.js
+++ client/scripts.js
@@ -1,7 +1,7 @@
 //function to handle submit and page reload
 function handleSubmitReload(event){
     searchHandler(event)
-    loadResults(event)
+    //loadResults(event)
 }
 
 function searchHandler(event){
@@ -20,27 +20,13 @@ function searchHandler(event){
 }
 
 function sortResults(data) {
-    //loop over first 10 results
+    console.log(data)
+    //loop over first 10 results and add to final results object
     for(let i=0; i< 10; i++) {
-        //see if it has a cahce id:
-        if (data.items[i].cacheId = true){
-        //give result a name from cacheId
-            let a = data.items[i].cacheId
-            //make it a new class and add the data
-            a = new Result(`${data.items[i].title}`, `${data.items[i].snippet}`, `${data.items[i].formattedUrl}`);
+            let a = new Result(`${data.items[i].title}`, `${data.items[i].snippet}`, `${data.items[i].formattedUrl}`);
             finalResults[i] = a
-            
-        } else {
-            // give Result a name from metadata
-            let b = data.items[i].pagemap.thing.name
-
-            //make new class and add data
-            b = new Result(`${data.items[i].title}`, `${data.items[i].snippet}`, `${data.items[i].formattedUrl}`);
-            
-            //add results object to final result object
-            finalResults[i] = b
-        }
     } 
+    console.log(finalResults)
 }
 
 // class to put results in 
@@ -54,18 +40,40 @@ class Result {
 
 //object for all the results to be wrapped in
 let finalResults = {}
+let finalResult;
 
 
 //RELOAD PAGE FUNC
 function luckyWebsiteReroute(event) {
     event.preventDefault()
-    searchHandler(event)
-    let newPage = finalResults[0].formattedUrl
-    window.location.replace(`${newPage}`);
-
+    searchHandlerOne(event)
+    window.location.replace(`${finalResult}`);
 }
 
-//function to chose top one from the object
+//searches for one result
+function searchHandlerOne(event){
+    event.preventDefault();
+    //get search value
+    let search = document.getElementById('searchInput').value;
+
+    //create the url
+    let url = ('https://www.googleapis.com/customsearch/v1?key=' + API_KEY + '&cx=' + cx + '&q=' + search)
+    fetch(url)  
+    .then(response => response.json())
+
+    //call function to get the specific result needed
+    .then(sortResult)
+    .catch(error => console.log(error))
+}
+
+//gives back top url
+function sortResult(data) {
+    //get top result
+    finalResult = `${data.items[i].formattedUrl}`
+} 
+
+
+
 
 
 
@@ -76,8 +84,8 @@ let API_KEY = 'AIzaSyBLQ6G4cY74__pZ0pfUiBV7qlBgxbssheo'
 //watch for submission of form
 const form = document.getElementById('form');
 form.addEventListener('submit', handleSubmitReload);
-form.addEventListener('click', luckyWebsiteReroute )
+//form.addEventListener('click', luckyWebsiteReroute )
 
-module.exports = {
-    searchHandler
-}
+//module.exports = {
+    //searchHandler
+//}
